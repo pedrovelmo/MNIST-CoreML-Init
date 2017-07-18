fe5a80fab6d5a916e76bdc62d7b7229cdbe8de69
Pedro Velmovitsky <pedrovelmo@gmail.com>
2017-07-18 12:24:55 -0300

    second commit

--- CoreML_MNIST/ViewController.swift
+++ CoreML_MNIST/ViewController.swift
@@ -12,7 +12,35 @@ import Vision
 import ImageIO
 
 class ViewController: UIViewController {
-
+    var lastPoint = CGPoint.zero
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
+    var inputImage: CIImage!
+    
+    
+    @IBOutlet weak var tempImageView: UIImageView!
+    @IBOutlet weak var classificationLabel: UILabel!
+    @IBAction func takePicture(_ sender: Any) {
+    // Get image from view
+        UIGraphicsBeginImageContext(tempImageView.frame.size)
+        tempImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        guard let img = UIGraphicsGetImageFromCurrentImageContext() else { fatalError("no image from drawing") }
+        classificationLabel.text = "Analyzing image"
+        guard let ciImage = CIImage(image: img)
+            else { fatalError("can't create CIImage from UIImage") }
+        
+        let orientation = CGImagePropertyOrientation(rawValue: UInt32(img.imageOrientation.rawValue))
+        inputImage = ciImage.applyingOrientation((orientation?.rawValue as? Int32)!)
+        
+         UIGraphicsEndImageContext()
+        // MARK: Uncomment this code to save photo to photo album
+        // UIImageWriteToSavedPhotosAlbum(img!, nil, nil, nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
