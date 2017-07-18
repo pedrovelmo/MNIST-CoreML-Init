1085ebb480752733f1cf7299ea78d5d52a46bd4f
Pedro Velmovitsky <pedrovelmo@gmail.com>
2017-07-18 11:54:17 -0300

    initial commit

---
+++ CoreML_MNIST/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CoreML_MNIST
+//
+//  Created by Pedro Velmovitsky on 18/07/17.
+//  Copyright © 2017 velmovitsky. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+import ImageIO
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
